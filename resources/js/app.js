72c4a8b3726458748de1af03857f3d0348fb0f61
nthnyvllflrs <anthonvillaflor@gmail.com>
2020-09-25 16:48:48 +0800

    vue integration

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+import router from './plugins/router.js'
+
+const app = new Vue({
+    router,
+}).$mount('#app')
