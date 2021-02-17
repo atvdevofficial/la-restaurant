f6720dadedda44f975c8411d00a945418b49b4f0
nthnyvllflrs <anthonvillaflor@gmail.com>
2021-02-17 13:22:04 +0800

    refactor: changed VerificationController

--- app/Http/Controllers/VerificationController.php
+++ app/Http/Controllers/VerificationController.php
@@ -12,63 +12,86 @@ class VerificationController extends Controller
 {
     use ITextMo;
 
+    /**
+     * Verify contact number
+     *
+     * @param  \App\Http\Requests\VerifyContactNumberRequest $request
+     * @return \Illuminate\Http\Response
+     */
     public function verifyContactNumber(VerifyContactNumberRequest $request)
     {
-        $contactNumber = $request->validated()['contact_number'];
-        $verificationCode = rand(111111, 999999);
-        $message = 'Your ' . env('APP_NAME', 'Template') . ' new customer verification code is ' . $verificationCode;
+        // $contactNumber = $request->validated()['contact_number'];
+        // $verificationCode = rand(111111, 999999);
+        // $message = 'Your ' . env('APP_NAME', 'PitStop') . ' new user verification code is ' . $verificationCode;
 
-        $iTextMoResponse = $this->sendSMS($contactNumber, $message);
+        // $iTextMoResponse = $this->sendSMS($contactNumber, $message);
 
-        if ($iTextMoResponse == '0') return response(['code' => $verificationCode]);
-        else return response(['message' => 'Something Went Wrong'], 422);
+        // if ($iTextMoResponse == '0') return response(['code' => $verificationCode]);
+        // else return response(['message' => 'Something Went Wrong', 'code' => $iTextMoResponse], 500);
     }
 
+    /**
+     * Send generated code to contact number
+     *
+     * @param  \App\Http\Requests\ForgotPasswordRequest $request
+     * @return \Illuminate\Http\Response
+     */
     public function sendCodeToContactNumber(ForgotPasswordRequest $request)
     {
-        $contactNumber = $request->validated()['contact_number'];
-        $verificationCode = rand(111111, 999999);
-        $message = 'Your ' . env('APP_NAME', 'Template') . ' forgot password verification code is ' . $verificationCode;
+        // $contactNumber = $request->validated()['contact_number'];
+        // $verificationCode = rand(111111, 999999);
+        // $message = 'Your ' . env('APP_NAME', 'PitStop') . ' forgot password user verification code is ' . $verificationCode;
 
-        $iTextMoResponse = $this->sendSMS($contactNumber, $message);
-        $customer = $this->customerRepository->showByContactNumber($contactNumber);
-        $user = $this->userRepository->show($customer->user_id);
-        $this->userRepository->update($user->id, ['code' => $verificationCode]);
+        // $iTextMoResponse = $this->sendSMS($contactNumber, $message);
 
-        if ($iTextMoResponse == '0') return response(['code' => $verificationCode]);
-        else return response(['message' => 'Something Went Wrong'], 422);
+        // $rider = Rider::whereContactNumber($contactNumber);
+        // $rider->user->update(['code' => $verificationCode]);
+
+        // if ($iTextMoResponse == '0')
+        //     return response(['code' => $verificationCode]);
+        // else
+        //     return response(['message' => 'Something Went Wrong', 'code' => $iTextMoResponse], 500);
     }
 
+    /**
+     * Verify code for change password request
+     *
+     * @param  \App\Http\Requests\VerifyCodeRequest $request
+     * @return \Illuminate\Http\Response
+     */
     public function verifyForgotPasswordCode(VerifyCodeRequest $request)
     {
-        $contactNumber = $request->validated()['contact_number'];
-        $verificationCode = $request->validated()['code'];
+        // $contactNumber = $request->validated()['contact_number'];
+        // $verificationCode = $request->validated()['code'];
 
-        $customer = $this->customerRepository->showByContactNumber($contactNumber);
-        $user = $this->userRepository->show($customer->user_id);
+        // $rider = Rider::whereContactNumber($contactNumber);
+        // $riderUser = $rider->user;
 
-        if ($user->code == $verificationCode) return response(null);
-        else return response(null, 401);
+        // if ($riderUser->code == $verificationCode)
+        //     return response(['message' => 'Verification Code Match'], 200);
+        // else
+        //     return response(['message' => 'Verification Code Mismatch', 422]);
     }
 
+    /**
+     * Change password of forgot password
+     *
+     * @param  \App\Http\Requests\VerifyCodeRequest $request
+     * @return \Illuminate\Http\Response
+     */
     public function forgotPasswordChangePassword(ChangePasswordRequest $request)
     {
-        $contactNumber = $request->validated()['contact_number'];
-        $verificationCode = $request->validated()['code'];
-        $newPassword = $request->validated()['password'];
+        // $contactNumber = $request->validated()['contact_number'];
+        // $verificationCode = $request->validated()['code'];
+        // $newPassword = $request->validated()['password'];
 
-        $customer = $this->customerRepository->showByContactNumber($contactNumber);
-        $user = $this->userRepository->show($customer->user_id);
+        // $rider = Rider::whereContactNumber($contactNumber);
+        // $riderUser = $rider->user;
 
-        if ($user->code == $verificationCode) {
-            $this->userRepository->update($user->id, [
-                'code' => null,
-                'password' => $newPassword,
-            ]);
-            return response([
-                'message' => 'Password Changed Successfully'
-            ]);
-        }
-        else return response(null, 401);
+        // if ($riderUser->code == $verificationCode) {
+        //     $riderUser->update(['code' => null, 'password' => $newPassword]);
+        //     return response(['message' => 'Password Changed Successfully']);
+        // } else
+        //     return response(null, 401);
     }
 }
