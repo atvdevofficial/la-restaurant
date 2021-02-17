a68a1bc2ce96b9d9c8cb8af7d4a116984fbb2c1c
nthnyvllflrs <anthonvillaflor@gmail.com>
2021-02-17 13:23:17 +0800

    feat: verification controller routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,11 +14,16 @@ use Illuminate\Http\Request;
 */
 
 Route::group(['middleware' => 'forceJsonResponse'], function () {
-    
+
     Route::group(['prefix' => 'v1'], function() {
 
         Route::post('/login', 'API\AuthController@login');
-       
+
+        Route::get('/verification', 'VerificationController@verifyContactNumber');
+        Route::put('/forgot-password', 'VerificationController@sendCodeToContactNumber');
+        Route::put('/forgot-password/verify-code', 'VerificationController@verifyForgotPasswordCode');
+        Route::put('/forgot-password/change-password', 'VerificationController@forgotPasswordChangePassword');
+
         Route::group(['middleware' => 'auth:api'], function() {
 
             Route::post('/logout', 'API\AuthController@logout');
