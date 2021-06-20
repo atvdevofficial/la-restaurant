ec92cc29aa9cfb500ef1bd9ceb69a32ebd025cb3
nthnyvllflrs <anthonvillaflor@gmail.com>
2021-06-20 22:01:40 +0800

    feat: added role to user model

--- app/User.php
+++ app/User.php
@@ -19,6 +19,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'email', 'password',
+        'role',
         'google_user_id'
     ];
 
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('role');
             $table->string('google_user_id')->nullable();
             $table->timestamps();
         });
